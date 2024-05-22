283680ef1344bbbe96b8015b1576f669f5ae0378
Deuphro <45364464+Deuphro@users.noreply.github.com>
2024-05-22 17:13:47 +0200

    Add files via upload

---
+++ XOP/rust-extension/pkg/attribrustor_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function compute(a: number, b: number): number;
+export function add(a: number, b: number): number;
+export function arrust(a: number, b: number, c: number): void;
+export function bench(a: number): number;
+export function sieve(a: number, b: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
